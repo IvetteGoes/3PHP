ccf5de67c52e85f4043495e87f6aaafe5996bbaf
IvetteGoes <ivette.tapatia@hotmail.com>
2014-10-02 17:46:39 -0500

    Correcion

--- 3.php
+++ 3.php
@@ -8,10 +8,13 @@
 <?php
 //Declarar variables
 $cantidad1=$_POST["cantidad1"];
+$descripcion1=$_POST["descripcion1"];
 $precio1=$_POST["precio1"];
 $cantidad2=$_POST["cantidad2"];
+$descripcion2=$_POST["descripcion2"];
 $precio2=$_POST["precio2"];
 $cantidad3=$_POST["cantidad3"];
+$descripcion3=$_POST["descripcion3"];
 $precio3=$_POST["precio3"];
 
 define('IVA',.16);
@@ -26,7 +29,10 @@ $iva=IVA*$subtotal;
 
 $total=$subtotal+$iva;
 
-echo "Total de Factura:";
+echo "Total de Factura:","<br>";
+echo $cantidad1,"	Piezas de: ", $descripcion1,"	Total: 	$", $multiplica1, "<br>";
+echo $cantidad2,"	Piezas de: ", $descripcion2,"	Total: 	$", $multiplica2, "<br>";
+echo $cantidad3,"	Piezas de: ", $descripcion3,"	Total: 	$", $multiplica3, "<br>";
 echo "Subtotal: ";
 echo $subtotal, "<br>";
 echo "IVA :";
